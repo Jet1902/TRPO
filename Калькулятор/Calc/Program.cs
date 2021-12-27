430dda8a2f6442abd1d2486704bed2fde6c27735
Jet1902 <53241503+Jet1902@users.noreply.github.com>
2021-12-27 23:44:10 +0300

    first

---
+++ Калькулятор/Calc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Operation:");
+                string op = Console.ReadLine();
+                Console.WriteLine("b:");
+                double b = Convert.ToDouble(Console.ReadLine());
+                double res = 0;
+                if (op == "*")
+                    res = a * b;
+                else if (op == "+")
+                    res = a + b;
+                else if (op == "-")
+                    res = a - b;
+                else if (op == "/")
+                    res = a / b;
+                Console.WriteLine(res);
+        }
+    }
+}
