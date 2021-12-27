3b02f902dc4d2c0d6ada6b96f5650dc2ab0ca323
Jet1902 <53241503+Jet1902@users.noreply.github.com>
2021-12-28 00:34:33 +0300

    feature/divByZero

--- Калькулятор/Calc/Program.cs
+++ Калькулятор/Calc/Program.cs
@@ -6,6 +6,9 @@ namespace Calc
     {
         static void Main(string[] args)
         {
+            int option = 0;
+            while (option != 1)
+            {
                 Console.WriteLine("a: ");
                 double a = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Operation:");
@@ -20,8 +23,16 @@ namespace Calc
                 else if (op == "-")
                     res = a - b;
                 else if (op == "/")
-                    res = a / b;
+				{
+					if(b == 0)
+						Console.WriteLine("Error: divide by zero");
+					else
+						res = a / b;
+				}
                 Console.WriteLine(res);
+                Console.WriteLine("Press 1 to exit... ");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
